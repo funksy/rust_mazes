4783d271e2ff6794875d803674cdf9de3c2f852b
John Lukich <john.p.lukich@gmail.com>
2025-01-09 15:51:50 -0700

    button runs callback function to make maze from grid

--- src/solver_algorithms/breadth_first_search.rs
+++ src/solver_algorithms/breadth_first_search.rs
@@ -2,9 +2,8 @@ use std::collections::{HashMap, VecDeque};
 
 use crate::maze::Maze;
 use crate::cell::Coord;
-use crate::maze_renderer::{MazeRenderer, CellState};
 
-pub fn find_solution(maze: &Maze, start: &Coord, finish: &Coord, renderer: &mut MazeRenderer) {
+pub fn find_solution(maze: &Maze, start: &Coord, finish: &Coord) {
     let mut frontier: VecDeque<Coord> = VecDeque::new();
     let mut explored: HashMap<Coord, Coord> = HashMap::new();
 
@@ -15,20 +14,20 @@ pub fn find_solution(maze: &Maze, start: &Coord, finish: &Coord, renderer: &mut 
 
         // renderer.update_cell_state(current_cell_y, current_cell_x, CellState::Explored);
 
-        add_adjacent_cells(&maze, &mut explored, &mut frontier, &current_cell, renderer);
+        add_adjacent_cells(&maze, &mut explored, &mut frontier, &current_cell);
     }
 
-    renderer.update_cell_state(start, CellState::Start);
-    renderer.update_cell_state(finish, CellState::Finish);
+    // renderer.update_cell_state(start, CellState::Start);
+    // renderer.update_cell_state(finish, CellState::Finish);
 
     let mut solution_cell = *explored.get(&finish).unwrap();
     while solution_cell != *start {
-        renderer.update_cell_state(&solution_cell, CellState::Solution);
+        // renderer.update_cell_state(&solution_cell, CellState::Solution);
         solution_cell = *explored.get(&solution_cell).unwrap();
     }
 }
 
-fn add_adjacent_cells(maze: &Maze, explored: &mut HashMap<Coord, Coord>, frontier: &mut VecDeque<Coord>, current_cell: &Coord, renderer: &mut MazeRenderer) {
+fn add_adjacent_cells(maze: &Maze, explored: &mut HashMap<Coord, Coord>, frontier: &mut VecDeque<Coord>, current_cell: &Coord) {
     let cell = maze.get_cell_ref(current_cell);
     if !cell.walls()[0] && !explored.contains_key(&Coord{ y: cell.coord().y - 1, x: cell.coord().x }) {
         explored.insert(Coord{ y: cell.coord().y - 1, x: cell.coord().x }, current_cell.clone());
