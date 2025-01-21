e4fae7073568860196c7713f7c58e763f3616187
John Lukich <john.p.lukich@gmail.com>
2025-01-20 20:42:05 -0700

    algorithm partially implemented

--- src/solver_algorithms/djikstras.rs
+++ src/solver_algorithms/djikstras.rs
@@ -1,16 +1,36 @@
-use std::collections::{HashMap};
+use std::collections::{HashMap, BinaryHeap};
+use std::cmp::Ordering;
 
 use dioxus::prelude::*;
 
 use crate::maze::Maze;
-use crate::cell::Coord;
+use crate::cell::{CellState, Coord};
 use crate::solver_algorithms::solver_helpers::SolverStatus;
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct DistanceToStart {
+    cell_coord: Coord,
+    distance: usize,
+}
+
+impl Ord for DistanceToStart {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.distance.cmp(&self.distance)
+    }
+}
+
+impl PartialOrd for DistanceToStart {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 pub struct Djikstras {
     start: Coord,
     finish: Coord,
     explored: HashMap<Coord, Coord>,
-    current_cell: Coord,
+    frontier: BinaryHeap<DistanceToStart>,
+    current_cell: DistanceToStart,
     pub status: SolverStatus,
 }
 
@@ -20,7 +40,11 @@ impl Djikstras {
             start: start.clone(),
             finish: finish.clone(),
             explored: HashMap::new(),
-            current_cell: start.clone(),
+            frontier: BinaryHeap::new(),
+            current_cell: DistanceToStart {
+                cell_coord: start.clone(),
+                distance: 0,
+            },
             status: SolverStatus::Initialized,
         }
     }
@@ -29,12 +53,32 @@ impl Djikstras {
         let maze: &mut Maze = &mut maze.write();
 
         match self.status {
-            SolverStatus::Initialized => {}
-            SolverStatus::InProgress => {}
+            SolverStatus::Initialized => {
+                maze.visit_cell(&self.start);
+                self.frontier.push(DistanceToStart {
+                    cell_coord: self.start,
+                    distance: 0,
+                });
+                self.status = SolverStatus::InProgress;
+            }
+            SolverStatus::InProgress => {
+                if self.current_cell.cell_coord != self.finish {
+                    self.current_cell = self.frontier.pop().unwrap();
+                    // TODO collect valid neighbor cells
+                    // TODO add entries to explored and frontier
+                } else {
+                    self.status == SolverStatus::Solved;
+                }
+            }
             SolverStatus::Solved => {}
             SolverStatus::Done => {
                 panic!("You shouldn't be here");
             }
         }
+    }
+
+    fn get_valid_neighbors(&self, maze: &Maze) -> Vec<Coord> {
+        let mut valid_neighbors: Vec<Coord> = Vec::new();
+        
     }
 }
