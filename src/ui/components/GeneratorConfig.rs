647e2a184e1bc4d75dda108ed09a8f09e73dc2d9
John Lukich <john.p.lukich@gmail.com>
2025-01-26 13:14:07 -0700

    consolidated UI elements

--- src/ui/components/GeneratorConfig.rs
+++ src/ui/components/GeneratorConfig.rs
@@ -4,7 +4,13 @@ use crate::ui::components::Dropdown::Dropdown;
 use crate::ui::components::NumInput::NumInput;
 
 #[component]
-pub fn GeneratorConfig(dropdown_options: Vec<(String, String)>, generator_algo_choice: Signal<String>, generated: bool, generator_delay: Signal<usize>) -> Element {
+pub fn GeneratorConfig(
+    dropdown_options: Vec<(String, String)>,
+    height: Signal<usize>, width: Signal<usize>,
+    generator_algo_choice: Signal<String>,
+    disabled: bool,
+    generator_delay: Signal<usize>
+) -> Element {
     rsx! {
         form {
             fieldset {
@@ -16,7 +22,28 @@ pub fn GeneratorConfig(dropdown_options: Vec<(String, String)>, generator_algo_c
                     options: dropdown_options,
                     helper_text: "Maze Generator Algo".to_string(),
                     value: generator_algo_choice,
-                    disabled: generated,
+                    disabled: disabled,
+                }
+                div {
+                    id: "height-config",
+                    label { for: "height-input", "Height:" },
+                    NumInput {
+                        id: "height-input",
+                        value: height,
+                        max_val: 200,
+                        min_val: 2,
+                    }
+                }
+
+                div {
+                    id: "width-config",
+                    label { for: "width-input", "Width:" },
+                    NumInput {
+                        id: "width-input",
+                        value: width,
+                        max_val: 200,
+                        min_val: 2,
+                    }
                 }
                 label { for: "generator-delay-config", "Render Delay (ms)" },
                 NumInput {
