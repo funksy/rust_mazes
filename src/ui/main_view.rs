bc96de3e6dc55f506d3e15d09c2888401d61f0b4
John Lukich <john.p.lukich@gmail.com>
2025-02-17 12:51:32 -0700

    testing

--- src/ui/main_view.rs
+++ src/ui/main_view.rs
@@ -8,6 +8,7 @@ pub fn launch_app() {
 }
 
 static CSS: Asset = asset!("src/ui/assets/main.css");
+// static favicon: Asset = asset!("src/ui/assets/favicon.ico");
 
 fn App() -> Element {
     let maze: Signal<Maze> = use_signal(|| Maze::new(15, 15));
@@ -18,6 +19,13 @@ fn App() -> Element {
         document::Stylesheet { href: CSS }
         style { "@import url('https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap');" }
 
+        head {
+            link {
+                rel: "icon",
+                "type": "image/x-icon",
+                // href: favicon;
+            }
+        }
         div {
             id: "sidebar",
             h1 { "Mazer" },
